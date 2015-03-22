fdc1d441a03cda9922e764286e295e1089e623af
zamud <ricardo-zamudio@uiowa.edu>
2015-03-22 09:42:41 -0500

    public views working

--- HMSite/urls.py
+++ HMSite/urls.py
@@ -3,9 +3,7 @@ from django.contrib import admin
 
 
 urlpatterns = patterns('',
-    # Examples:
-     url(r'^$', 'HMSite.HMS.views.homepage', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+    url(r'HMS/', include ('HMS.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
 )
