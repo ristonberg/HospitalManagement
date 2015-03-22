02d7e4071b234834a10a5d0379e0bceda690a1a3
zamud <ricardo-zamudio@uiowa.edu>
2015-03-22 14:38:33 -0500

    login, registration HTML working, not CSS or JS

--- HMS/views.py
+++ HMS/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
+from django.template import RequestContext, loader
 
 def login(request):
-    return HttpResponse("Login")
+    return render(request, 'HMS/Login/login.html')
 
 def registration(request):
-    return HttpResponse("Registration")
+    return render(request, 'HMS/Registration/registration.html')
 
 def homepage(request):
     return HttpResponse("Homepage")
+
