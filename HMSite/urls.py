75eec97e6038b0f01738ffda6e6cb6c7c68bc892
zamud <ricardo-zamudio@uiowa.edu>
2015-03-21 21:53:12 -0500

    rick update

--- HMSite/urls.py
+++ HMSite/urls.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 
 urlpatterns = patterns('',
     # Examples:
-     url(r'^$', 'HMSite.HMS.views.home', name='home'),
+     url(r'^$', 'HMSite.HMS.views.homepage', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
